3015bac1e4df2641c890051bb88d76a43d67a92b
salmonavocadocrabstick <iLoveSalmon!1>
2018-12-31 17:41:28 +0800

    Input loop
    Setting up input loop for user

--- scraping_project.py
+++ scraping_project.py
@@ -5,27 +5,26 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+def went_to_college(desc):
+	went_to_college = any([word == "University" or "College" for word in desc])
+	if went_to_college is True:
+		return f"This person went to college or university."
+	return f"This person did not go to college or university."
+
+
 url = "http://quotes.toscrape.com"
 req = requests.get(url)
-#print(req.text)
-
 soup = BeautifulSoup(req.text, "html.parser")
-#print(soup)
 
-quote = soup.find(class_="text").get_text() # Quote only, no other information
-#print(quote)
-
+# Get quote, author, about URL
+quote = soup.find(class_="text").get_text() 
 author = soup.find(class_="author").get_text() 
-#print(author)
-
 about_url = url+soup.find(class_="author").find_next_sibling()["href"]
-#print(about_url)
 
-# First initial
+
+# First , last name initials
 hint_1 = f"This person's first name begins with the letter '{author[0]}'"
-#print(hint_1)
-
-# Last name initial
 last_ini = [ char for char in author[1:-1] if char.isupper()] # Iterate string backwards, return first uppercase letter
 hint_2 = f"This person's first name begins with the letter '{last_ini}'"
 
@@ -36,19 +35,24 @@ about_soup = BeautifulSoup(about_req.text, "html.parser")
 
 # Birthday and Birthplace
 birthday = about_soup.find(class_="author-born-date")
-# need get_text() later
-
 birthplace = birthday.find_next_sibling()
-#print(hint_3_birthplace.get_text())
-
 hint_3 = f"This person was born on {birthday.get_text()}, {birthplace.get_text()}."
 
 # Schooling
-hint_4 = about_soup.find(class_="author-description").get_text()
-#print(type(hint_4))
-went_to_college = any([word == "University" or "College" for word in hint_4])
-if went_to_college is True:
-	print(f"This person went to college or university.")
-else:
-	print(f"This person did not go to college or university.")
+desc = about_soup.find(class_="author-description").get_text()
+hint_4 = went_to_college(desc)
 
+#----------------------------------------------------------------
+
+chance = 4
+again = "Y"
+
+guess = input("Who said this?\n" + quote + "Take a guess! \n\n")
+while chance is not 0 and again == "Y":
+	if guess == author:
+		print("Congrats! You got it!")
+		again = input("Play again? Y/N\n\n").upper()
+	else:
+		print("Not quite. Here's a hint.\n" + hint_1)
+		guess = input("Guess again.\n\n")
+		chance -= 1
