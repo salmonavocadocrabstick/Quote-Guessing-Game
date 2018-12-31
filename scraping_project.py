521bc2b0260ad0d341685db2ddf8a207482889c7
salmonavocadocrabstick <iLoveSalmon!1>
2018-12-31 18:57:29 +0800

    Changing hint 4
    Hint 4 logic is not working out

--- scraping_project.py
+++ scraping_project.py
@@ -6,30 +6,23 @@ import requests
 from bs4 import BeautifulSoup
 from random import choice
 
-
-def went_to_college(desc):
-	went_to_college = any([word == "University" or "College" for word in desc])
-	if went_to_college is True:
-		return f"This person went to college or university."
-	return f"This person did not go to college or university."
-
 def make_hints(author, about_soup):
 	hints = []
 	# First , last name initials
 	hints.append(f"This person's first name begins with the letter '{author[0]}'")
-	last_ini = [ char for char in author[1:-1] if char.isupper()] # Iterate string backwards, return first uppercase letter
-	hints.append(f"This person's first name begins with the letter '{last_ini}'")
+	last_ini = [char for char in author[1:-1] if char.isupper()] # Iterate string backwards, return first uppercase letter
+	hints.append(f"This person's last name begins with the letter '{last_ini[-1]}'")
 
 	# Birthday and Birthplace
 	birthday = about_soup.find(class_="author-born-date")
 	birthplace = birthday.find_next_sibling()
 	hints.append(f"This person was born on {birthday.get_text()}, {birthplace.get_text()}.")
 
-	# Schooling
-	desc = about_soup.find(class_="author-description").get_text()
-	hints.append(went_to_college(desc))
-
-	return hints
+	# Acheivment
+	desc = str(about_soup.find(class_="author-description").get_text()).split('.')
+	info = list(filter(lambda x : x == "His" or x == "Her", desc))
+	print(info)
+	#return hints
 
 def give_hint(hints):
 	hint = choice(hints)
@@ -46,7 +39,7 @@ url = "http://quotes.toscrape.com"
 req = requests.get(url)
 soup = BeautifulSoup(req.text, "html.parser")
 
-#Variables for looping
+# #Variables for looping
 again = "Y"
 
 # Get quote, author, about URL
@@ -65,23 +58,23 @@ while again != "N":
 	# Make hints with the above info
 	hints = make_hints(author.get_text(), about_soup)
 
-	# Game start
-	guess = input("Who said this?\n" + quote.get_text() + "Take a guess!(You have 4 chances.) \n\n")
-	chance = 4
-	while chance is not 0 and again == "Y":
-		if guess == author.get_text():
-			print("Congrats! You got it!")
-			break
-		else:
-			hint = give_hint(hints)
-			print("Not quite. Here's a hint.\n" + hint)
-			guess = input("Guess again.\n\n")
-			hints = update_hint_list(hint, hints)
-			chance -= 1
+# 	# Game start
+# 	guess = input("Who said this?\n" + quote.get_text() + "Take a guess!(You have 4 chances.) \n\n")
+# 	chance = 4
+# 	while chance is not 0 and again == "Y":
+# 		if guess == author.get_text():
+# 			print("Congrats! You got it!")
+# 			break
+# 		else:
+# 			hint = give_hint(hints)
+# 			print("Not quite. Here's a hint.\n" + hint)
+# 			guess = input("Guess again.\n\n")
+# 			hints = update_hint_list(hint, hints)
+# 			chance -= 1
 
-		if chance == 0:
-			print(f"No more chances left! The answer : {author.get_text()}.  ")
+# 		if chance == 0:
+# 			print(f"No more chances left! The answer : {author.get_text()}.  ")
 
-	# Point to the next set of elements
-	again = input("Do you want to play again? Y/N\n\n ").upper()
-	quote = quote.find_parent().find_next_sibling().find(class_="text")
+# 	# Point to the next set of elements
+# 	again = input("Do you want to play again? Y/N\n\n ").upper()
+# 	quote = quote.find_parent().find_next_sibling().find(class_="text")
