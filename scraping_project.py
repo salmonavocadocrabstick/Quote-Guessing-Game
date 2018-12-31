aaa3d07558ecb3990150a21d643111e3916ea23f
salmonavocadocrabstick <iLoveSalmon!1>
2018-12-31 17:02:03 +0800

    Extracting first and last name initials
    Added a list comprehension for last name extraction

--- scraping_project.py
+++ scraping_project.py
@@ -12,5 +12,29 @@ req = requests.get(url)
 soup = BeautifulSoup(req.text, "html.parser")
 #print(soup)
 
-data_1 = soup.find(class_="quote").get_text()
-print(data_1)
+quote = soup.find(class_="text").get_text() # Quote only, no other information
+#print(quote)
+
+author = soup.find(class_="author").get_text() 
+#print(author)
+
+about_url = url+soup.find(class_="author").find_next_sibling()["href"]
+#print(about_url)
+
+#Getting the about
+about_req = requests.get(about_url)
+about_soup = BeautifulSoup(about_req.text, "html.parser")
+
+
+# First initial
+hint_1 = f"This person's first name begins with the letter '{author[0]}'"
+#print(hint_1)
+
+# Last name initial
+last_ini = [ char for char in author[1:-1] if char.isupper()] # Iterate string backwards, return first uppercase letter
+hint_1 = f"This person's first name begins with the letter '{last_ini}'"
+
+
+# Birthday and Birthplace
+
+# Schooling
