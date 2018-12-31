2f7038e4f9c462c8f767126b159230d2da85ed99
salmonavocadocrabstick <iLoveSalmon!1>
2018-12-31 18:09:15 +0800

    Completed one full round of the game
    Hints are now pushed into a list.
    Each time when a hint is given, the hint is removed from the list.

--- scraping_project.py
+++ scraping_project.py
@@ -4,6 +4,7 @@
 
 import requests
 from bs4 import BeautifulSoup
+from random import choice
 
 
 def went_to_college(desc):
@@ -11,6 +12,33 @@ def went_to_college(desc):
 	if went_to_college is True:
 		return f"This person went to college or university."
 	return f"This person did not go to college or university."
+
+def make_hints(author, about_soup):
+	hints = []
+	# First , last name initials
+	hints.append(f"This person's first name begins with the letter '{author[0]}'")
+	last_ini = [ char for char in author[1:-1] if char.isupper()] # Iterate string backwards, return first uppercase letter
+	hints.append(f"This person's first name begins with the letter '{last_ini}'")
+
+	# Birthday and Birthplace
+	birthday = about_soup.find(class_="author-born-date")
+	birthplace = birthday.find_next_sibling()
+	hints.append(f"This person was born on {birthday.get_text()}, {birthplace.get_text()}.")
+
+	# Schooling
+	desc = about_soup.find(class_="author-description").get_text()
+	hints.append(went_to_college(desc))
+
+	return hints
+
+def give_hint(hints):
+	hint = choice(hints)
+	return hint
+
+def update_hint_list(hint, hints):
+	hints.remove(hint)
+	return hints
+
 
 
 url = "http://quotes.toscrape.com"
@@ -22,40 +50,33 @@ quote = soup.find(class_="text").get_text()
 author = soup.find(class_="author").get_text() 
 about_url = url+soup.find(class_="author").find_next_sibling()["href"]
 
-
-# First , last name initials
-hint_1 = f"This person's first name begins with the letter '{author[0]}'"
-last_ini = [ char for char in author[1:-1] if char.isupper()] # Iterate string backwards, return first uppercase letter
-hint_2 = f"This person's first name begins with the letter '{last_ini}'"
-
-
 #Getting the about
 about_req = requests.get(about_url)
 about_soup = BeautifulSoup(about_req.text, "html.parser")
 
-# Birthday and Birthplace
-birthday = about_soup.find(class_="author-born-date")
-birthplace = birthday.find_next_sibling()
-hint_3 = f"This person was born on {birthday.get_text()}, {birthplace.get_text()}."
-
-# Schooling
-desc = about_soup.find(class_="author-description").get_text()
-hint_4 = went_to_college(desc)
+# Make hints with the above info
+hints = make_hints(author, about_soup)
 
 #----------------------------------------------------------------
 
+#Variables for looping
 chance = 4
 again = "Y"
 
 while again != "N":
-	guess = input("Who said this?\n" + quote + "Take a guess! \n\n")
+	guess = input("Who said this?\n" + quote + "Take a guess!(You have 4 chances.) \n\n")
 	while chance is not 0 and again == "Y":
 		if guess == author:
 			print("Congrats! You got it!")
 			break
 		else:
-			print("Not quite. Here's a hint.\n" + hint_1)
+			hint = give_hint(hints)
+			print("Not quite. Here's a hint.\n" + hint)
 			guess = input("Guess again.\n\n")
+			hints = update_hint_list(hint, hints)
 			chance -= 1
+
+		if chance == 0:
+			print(f"No more chances left! The answer : {author}.  ")
 	again = input("Do you want to play again? Y/N\n\n ").upper()
 	chance = 4
