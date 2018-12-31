8d7963b55be6ddbdc1b4dd96bbc6e7ca372e1340
salmonavocadocrabstick <iLoveSalmon!1>
2018-12-31 16:38:29 +0800

    First Quote
    Scraped the first quote

--- scraping_project.py
+++ scraping_project.py
@@ -3,7 +3,14 @@
 # and generates a guessing game.
 
 import requests
+from bs4 import BeautifulSoup
 
 url = "http://quotes.toscrape.com"
 req = requests.get(url)
-print(req.text)
+#print(req.text)
+
+soup = BeautifulSoup(req.text, "html.parser")
+#print(soup)
+
+data_1 = soup.find(class_="quote").get_text()
+print(data_1)
